4fcba03c2c05e8f2c2907ac11f53edaab132769b
Kham <piotr.szeremeta@gmail.com>
2013-08-02 00:19:47 +0200

    changed find_site_name to use match_from_hash, now it's half decent

--- app/models/video_address.rb
+++ app/models/video_address.rb
@@ -5,19 +5,22 @@ module VideoAddress
   attr_reader :store
 
   def self.find_site_name address
-    sites = select_key(store, 'matcher_name').select{|ident, regex| address.match(regex)}.keys
+    sites = match_from_hash(address, store, 'matcher_name').keys
     return sites.first    if sites.size == 1
     return "other"        if sites.empty?
     raise ManySites
   end
 
-  def self.select_key hash, key
-    new_hash = {}
-    hash.each{|k,v| new_hash[k] = v[key]}
-    new_hash
+  def self.match_from_hash string, hash, inner_key=nil
+    hash.select do |ident, data| 
+      matcher = inner_key ? data[inner_key] : data
+      string.match(matcher)
+    end
   end
 
   def self.store
-    @store ||= HashWithIndifferentAccess.new YAML.load(File.read('./lib/assets/video_sites.yml'))
+    @store ||= HashWithIndifferentAccess
+      .new YAML
+      .load( File.read('./lib/assets/video_sites.yml'))
   end
 end
