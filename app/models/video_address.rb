b50a04e9d082ec14fbdaed56e840c4c010dc166d
Kham <piotr.szeremeta@gmail.com>
2013-08-02 00:09:22 +0200

    altered find_site_name to use helper select_key. Not sure if good

--- app/models/video_address.rb
+++ app/models/video_address.rb
@@ -2,19 +2,22 @@ module VideoAddress
 
   ManySites = Class.new(Exception)
 
-  KNOWN_SITES = {
-    youtube: /youtube/,
-    vimeo: /vimeo/
-  }
+  attr_reader :store
 
   def self.find_site_name address
-    sites = KNOWN_SITES.select{|ident, reg| address.match(reg)}.keys
+    sites = select_key(store, 'matcher_name').select{|ident, regex| address.match(regex)}.keys
     return sites.first    if sites.size == 1
     return "other"        if sites.empty?
     raise ManySites
   end
 
+  def self.select_key hash, key
+    new_hash = {}
+    hash.each{|k,v| new_hash[k] = v[key]}
+    new_hash
+  end
+
   def self.store
-    HashWithIndifferentAccess.new YAML.load(File.read('./lib/assets/video_sites.yml'))
+    @store ||= HashWithIndifferentAccess.new YAML.load(File.read('./lib/assets/video_sites.yml'))
   end
 end
--- spec/models/video_address_spec.rb
+++ spec/models/video_address_spec.rb
@@ -1,10 +1,10 @@
 describe VideoAddress do
 	it ".find_site_name retruns generic site name from address" do
 		address = 'http://www.vimeo.com/2324323243'
-		VideoAddress.find_site_name(address).should eq(:vimeo)
+		VideoAddress.find_site_name(address).should eq('vimeo')
 		address = 'http://www.youtube.com/1324'
-		VideoAddress.find_site_name(address).should  eq(:youtube)
+		VideoAddress.find_site_name(address).should  eq('youtube')
 		address = 'something different'
-		VideoAddress.find_site_name(address).should  eq(:other)
+		VideoAddress.find_site_name(address).should  eq('other')
 	end
 end
