ff3e21cbebb397952490da7e40ad682623cc6fe5
Kham <piotr.szeremeta@gmail.com>
2013-08-04 01:24:20 +0200

    Revert "added facebook login"
    This reverts commit ac4cf82a2ad70cae8ea47c1031d720d42a19eaf3.

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,13 +8,4 @@ class User < ActiveRecord::Base
   def to_s
     nick
   end
-
-  def self.from_facebook data
-    info = data['info']
-    user = User.where(email: info['email']).first_or_initialize do |u|
-      u.nick = info['name']
-    end
-    user.save
-    user
-  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,6 @@ MediaShare::Application.routes.draw do
 
   resources :media
 
-  get '/auth/:provider/callback' => 'session#create'
-
 
   root to: "media#index" 
 end
