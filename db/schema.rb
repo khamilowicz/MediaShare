0b8974aecc35900b4c4546e2a2c2171429cab9c2
Kham <piotr.szeremeta@gmail.com>
2013-08-04 03:08:28 +0200

    added some files

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130803232917) do
+ActiveRecord::Schema.define(version: 20130804001733) do
 
   create_table "media", force: true do |t|
     t.string   "name"
@@ -39,6 +39,7 @@ ActiveRecord::Schema.define(version: 20130803232917) do
     t.string   "provider"
     t.string   "uid"
     t.string   "name"
+    t.string   "image"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
